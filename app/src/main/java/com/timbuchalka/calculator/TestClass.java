9c106a762ad9ad3a94f6d82bd33495614b1e2a83
Tobiloba <ajayitobi11@gmail.com>
2019-03-19 15:40:57 +0100

    Important change to Test.java class

--- app/src/main/java/com/timbuchalka/calculator/TestClass.java
+++ app/src/main/java/com/timbuchalka/calculator/TestClass.java
@@ -2,6 +2,6 @@ package com.timbuchalka.calculator;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("The test");
+        System.out.println("The test test test");
     }
 }
